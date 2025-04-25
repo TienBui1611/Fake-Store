0e68f10ba26e3a592a4668945d35f4ee8778367d
unknown <tien.bui@griffithuni.edu.au>
2025-04-25 22:44:45 +1000

    Minor update for Assignment 2 Milestone 1

--- App.js
+++ App.js
@@ -5,18 +5,21 @@ import CategoryScreen from './src/screens/CategoryScreen';
 // import ProductListScreen from './src/screens/ProductListScreen'; // for later
 import ProductListScreen from './src/screens/ProductListScreen';
 import ProductDetailScreen from './src/screens/ProductDetailScreen';
+import { ThemeProvider } from '@rneui/themed';
 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Category">
-        <Stack.Screen name="Category" component={CategoryScreen} />
-        <Stack.Screen name="ProductList" component={ProductListScreen} />
-        <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Category">
+          <Stack.Screen name="Category" component={CategoryScreen} />
+          <Stack.Screen name="ProductList" component={ProductListScreen} />
+          <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
